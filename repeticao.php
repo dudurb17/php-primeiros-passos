e3d1f7bf7cb4f26df923314aa4c6dda333ad3acb
dudurb17 <eduardo.robettibedin@gmail.com>
2024-08-08 11:18:04 -0300

    multiplication table

--- repeticao.php
+++ repeticao.php
@@ -21,9 +21,18 @@ for ($i = 0; $i <= 15; $i++) {
 //break e continue
 
 //exercise
-
-for ($i = 0; $i <= 100; $i++) {
+//odd numbers
+for ($i = 0; $i <= 10; $i++) {
   if ($i % 2 != 0) {
     echo $i . PHP_EOL;
   }
+}
+
+//multiplication table 
+
+$num = 5;
+$cont = 0;
+while ($cont <= 10) {
+  echo $num * $cont . PHP_EOL;
+  $cont++;
 }
