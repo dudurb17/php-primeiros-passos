1553208678c9182ffe7ddca040eb952af01d35f9
dudurb17 <eduardo.robettibedin@gmail.com>
2024-08-08 10:41:22 -0300

    if and else if

--- decisoes.php
+++ decisoes.php
@@ -1,7 +1,12 @@
 <?php $idade = 21;
 $name = 'Eduardo';
-
+$numeroDePessoas = 2;
 echo "Você só pode entrar se tiver mais do que 18 anos" . PHP_EOL;
 if ($idade >= 18 && $name == 'Eduardo') {
   echo "Você tem mais de 18 anos. Pode entrar";
+} else {
+  if ($idade >= 16 && $numeroDePessoas > 1) {
+    echo 'Voce tem 16 ou mais e esta acompanhado, então pode entrar';
+  } else
+    echo "Você não pode entrar";
 }
