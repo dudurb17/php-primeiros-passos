a7300607e02f1fdd847f22d84973024d0cfd351a
dudurb17 <eduardo.robettibedin@gmail.com>
2024-08-08 08:38:32 -0300

    first commit

---
+++ operacoes.php
@@ -0,0 +1,10 @@
+<?php $idade = 19;
+$idadeDaqui10Anos = $idade + 10;
+
+echo $idadeDaqui10Anos;
+$soma = 2 + 2;
+$subtracao = 2 - 2;
+$multiplicacao = 2 * 2;
+$divisao = 2 / 2;
+$doisAoCubo = 2 ** 3;
+$restoDaDivisao = 10 % 3;
